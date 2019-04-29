e25f3fdd0bd9e87a5d726560caa9847fd66f676b
Shannon Silver <lineshan@msu.edu>
2019-04-28 18:13:56 -0700

    Updates

---
+++ precache-manifest.63150e3fc32b8762bfc003b5987897a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c3b517325d5e00cd074da79219b321",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23a70db5ee42b2c273f1",
+    "url": "/static/css/main.b5ac3e7a.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "23a70db5ee42b2c273f1",
+    "url": "/static/js/main.7b4d9791.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
